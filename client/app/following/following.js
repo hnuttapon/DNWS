4ffb0adc355cea7776fe73aa3e4c0a5b13f4cbb9
NUTTAPON HANKAMOLSIRI <nuttapon_h@cmu.ac.th>
2019-05-14 19:30:56 +0700

    Finish

--- client/app/following/following.js
+++ client/app/following/following.js
@@ -13,5 +13,23 @@ angular.module('followingList', ['ngRoute'])
       $http.get('http://localhost:8080/twitterapi/following/', requestOptions).then(function (response) {
         self.followings = response.data;
       });
+
+        self.sendfollowing = function sendfollowing(followingname)
+      {
+        
+            const data = "followingname=" + encodeURIComponent(followingname);
+          $http.post('http://localhost:8080/twitterapi/following/', data, requestOptions).then(function (response) {
+              console.log(data);
+        });
+        }
+
+        self.sendunfollowing = function sendunfollowing(followingname) {
+
+            const data = "followingname=" + encodeURIComponent(followingname);
+            $http.delete('http://localhost:8080/twitterapi/following/?' + data, requestOptions).then(function (response) {
+                console.log(response);
+            });
+        }
+
     }]
 });
